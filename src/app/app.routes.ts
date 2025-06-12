6070ae31777fb0db04b4b4d23aeb0b9c3c4242ca
Shrikant <shrikantc60@gmail.com>
2025-06-12 20:36:28 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,10 @@ import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
 import { FeatureEvaluator } from './feature-evaluator/feature-evaluator';
 import { T1 } from './templates/t1/t1';
+import { T2 } from './templates/t2/t2';
 import { Templates } from './templates/templates';
+import { T3 } from './templates/t3/t3';
+import { T4 } from './templates/t4/t4';
 
 export const routes: Routes = [
   {
@@ -40,10 +43,43 @@ export const routes: Routes = [
       redirectTo: '1',
       pathMatch: 'full'
     }]
+  },{
+    path:'templates',
+    component: Templates,
+    children: [{
+      path: '2' ,
+      component: T2
+    },{
+      path:'',
+      redirectTo: '2',
+      pathMatch: 'full'
+    }]
+  },{
+    path:'templates',
+    component: Templates,
+    children:[{
+      path: '3',
+      component: T3
+    },{
+      path:'',
+      redirectTo: '3',
+      pathMatch: 'full'
+    }]
+  },{
+    path:'templates',
+    component: Templates,
+    children:[{
+      path: '4',
+      component: T4
+    },{
+      path:'',
+      redirectTo: '4',
+      pathMatch: 'full'
+    }]
   },
   {
     path: '',
     redirectTo: 'auth',
-    pathMatch: 'full',
+    pathMatch: 'full'
   },
 ];
